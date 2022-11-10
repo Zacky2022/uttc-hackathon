689340f766601afedfcaf3ae1e4848f7ba59e31d
Zacky2022 <harumiya322@g.ecc.u-tokyo.ac.jp>
2022-11-10 18:17:53 +0900

    80

--- BE/main.go
+++ BE/main.go
@@ -31,7 +31,7 @@ func main() {
 
 	// 8000番ポートでリクエストを待ち受ける
 	log.Println("Listening...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal(err)
 	}
 }
